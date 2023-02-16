509472cee229f6747cba09a386a044f7cc55d1a2
safaa khadim <khadim.safaa@gmail.com>
2023-02-16 10:21:13 +0100

    Create strange-advertising.py

---
+++ strange-advertising.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'viralAdvertising' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def viralAdvertising(n):
+    result = 5
+    cumul = 2
+    for i in range (1, n):
+        cumul = (cumul*3)//2
+        result = result + cumul
+    return result - 3 
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
